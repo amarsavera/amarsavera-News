6ac33780da971732e41d8f67c202a5ad69e21d74
amarsavera <info@saragone.in>
2026-06-26 15:51:47 +0530

    Config folder

---
+++ app.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+'app_name' => 'Amar Savera',
+
+'app_version' => '1.0.0',
+
+'timezone' => 'Asia/Kolkata',
+
+'maintenance_mode' => false,
+
+'debug' => false,
+
+'default_language' => 'hi',
+
+'default_role' => 'reporter',
+
+'default_status' => 'active'
+
+];
